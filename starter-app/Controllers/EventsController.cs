e0b1eea5db1ee044e7b5133c6757aac975fa6e40
krahman <kautsar.rahman@gmail.com>
2023-07-17 14:24:12 +0900

    feat: add EventsController and EventFormViewModel

---
+++ starter-app/Controllers/EventsController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace starter_app.Controllers
+{
+    public class EventsController : Controller
+    {
+        // GET: Events
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //GET: Events/New
+        public ActionResult New()
+        {
+            return View();
+        }
+    }
+}
---
+++ starter-app/ViewModels/EventFormViewModel.cs
@@ -0,0 +1,22 @@
+﻿using starter_app.Models;
+using System;
+using System.Collections.Generic;
+
+namespace starter_app.ViewModels
+{
+    public class EventFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public byte Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+        public DateTime DateTime
+        {
+            get
+            {
+                return DateTime.Parse(String.Format("{0} {1}", Date, Time));
+            }
+        }
+    }
+}
