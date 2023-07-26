93d5bb55f3538f41a3ec3c59aa4e54e0485e97f7
krahman <kautsar.rahman@gmail.com>
2023-07-26 15:43:49 +0900

    feat: add Attendances api Controller and webhost library

---
+++ starter-app/Controllers/Api/AttendancesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNet.Identity;
+using starter_app.Models;
+using System.Linq;
+using System.Web.Http;
+
+namespace starter_app.Controllers.Api
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _context { get; set; }
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // /api/Attendances/Attend
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody] int eventId)
+        {
+            var userId = User.Identity.GetUserId();
+
+            if (_context.Attendances.Any(a => a.AttendeeId == userId && a.EventId == eventId))
+                return BadRequest("the Attendance already exist");
+
+            var attendance = new Attendance()
+            {
+                EventId = eventId,
+                AttendeeId = userId
+            };
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
