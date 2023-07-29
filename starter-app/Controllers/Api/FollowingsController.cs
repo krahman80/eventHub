9de56d2df47097efc89238c381c1e3b1c2d5489b
krahman <kautsar.rahman@gmail.com>
2023-07-29 11:35:44 +0900

    feat: add following controller api & view

---
+++ starter-app/Controllers/Api/FollowingsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNet.Identity;
+using starter_app.Dtos;
+using starter_app.Models;
+using System.Linq;
+using System.Web.Http;
+
+namespace starter_app.Controllers.Api
+{
+    [Authorize]
+    public class FollowingsController : ApiController
+    {
+        public ApplicationDbContext _context { get; set; }
+
+        public FollowingsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Follow(FollowingDto dto)
+        {
+            var userId = User.Identity.GetUserId();
+
+            if (_context.Followings.Any(a => a.FollowerId == userId && a.FolloweeId == dto.FolloweeId))
+                return BadRequest("the Attendance already exist");
+
+            var following = new Following
+            {
+                FollowerId = userId,
+                FolloweeId = dto.FolloweeId
+            };
+
+            _context.Followings.Add(following);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
--- starter-app/Models/IdentityModels.cs
+++ starter-app/Models/IdentityModels.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Security.Claims;
@@ -13,6 +15,16 @@ namespace starter_app.Models
         [Required]
         [StringLength(100)]
         public string Name { get; set; }
+
+        public ICollection<Following> Followers { get; set; }
+        public ICollection<Following> Followees { get; set; }
+
+        public ApplicationUser()
+        {
+            Followers = new Collection<Following>();
+            Followees = new Collection<Following>();
+        }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -27,6 +39,7 @@ namespace starter_app.Models
 
         public DbSet<Event> Events { get; set; }
         public DbSet<Genre> Genres { get; set; }
+        public DbSet<Following> Followings { get; set; }
         public DbSet<Attendance> Attendances { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
@@ -44,6 +57,16 @@ namespace starter_app.Models
             modelBuilder.Entity<Attendance>()
                 .HasRequired(e => e.Event)
                 .WithMany()
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<ApplicationUser>()
+                .HasMany(u => u.Followers)
+                .WithRequired(f => f.Followee)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<ApplicationUser>()
+                .HasMany(u => u.Followees)
+                .WithRequired(f => f.Follower)
                 .WillCascadeOnDelete(false);
 
             base.OnModelCreating(modelBuilder);
